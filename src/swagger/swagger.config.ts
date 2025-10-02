92604282bfb8b6c75aa614e8bab32ebc8fa00c85
Projeto Integrador 2 <projetointegrador2.2025@gmail.com>
2025-10-02 06:41:10 -0300

    Delete src directory

--- src/swagger/swagger.config.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentBuilder } from '@nestjs/swagger';
-
-export const swaggerConfig = new DocumentBuilder()
-  .setTitle('Rotina Diária API')
-  .setDescription('API para gerenciamento de rotinas diárias')
-  .setVersion('1.0')
-  .addTag('users', 'Operações relacionadas aos usuários')
-  .addBearerAuth()
-  .build();
